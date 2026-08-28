d805e862344994ba4be86ac37936af71631dc889
Samuel-ctrl-create <fariassamuel885@gmail.com>
2026-08-28 01:44:59 -0300

    Add initial Main.lua for VØID HUB project
    Initial implementation of VØID HUB with features and start function.

---
+++ Main.lua
@@ -0,0 +1,36 @@
+-- VØID HUB
+-- Main.lua
+-- Open-source Roblox Studio project
+
+local VoidHub = {}
+
+VoidHub.Name = "VØID HUB"
+VoidHub.Version = "1.0.0"
+
+VoidHub.Features = {
+	"Fly",
+	"Speed",
+	"Jump Boost",
+	"Dash",
+	"Dash Power",
+	"Super Auto Click",
+	"Auto Farm",
+	"Auto Quest",
+	"Auto Collect",
+	"Auto Sell",
+	"Auto Fishing",
+	"Perfect Cast",
+	"Perfect Catch",
+	"ESP",
+	"Teleport",
+	"FPS Counter",
+	"Ping",
+	"Settings"
+}
+
+function VoidHub.Start()
+	print("VØID HUB iniciado!")
+	print("Versão: " .. VoidHub.Version)
+end
+
+return VoidHub
