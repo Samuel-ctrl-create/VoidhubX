2792fab3c0165c7b53c039ebee1815bc296356de
Samuel-ctrl-create <fariassamuel885@gmail.com>
2026-08-28 02:17:23 -0300

    Expand VoidHub features list
    Added new features to VoidHub including Anti AFK, Rejoin, Server Hop, Performance Mode, Graphics, Keybinds, UI Scale, Notifications, Theme, and Debug Mode.

--- Main.lua
+++ Main.lua
@@ -1,36 +1,31 @@
--- VØID HUB
--- Main.lua
--- Open-source Roblox Studio project
-
-local VoidHub = {}
-
-VoidHub.Name = "VØID HUB"
-VoidHub.Version = "1.0.0"
-
 VoidHub.Features = {
-	"Fly",
-	"Speed",
-	"Jump Boost",
-	"Dash",
-	"Dash Power",
-	"Super Auto Click",
-	"Auto Farm",
-	"Auto Quest",
-	"Auto Collect",
-	"Auto Sell",
-	"Auto Fishing",
-	"Perfect Cast",
-	"Perfect Catch",
-	"ESP",
-	"Teleport",
-	"FPS Counter",
-	"Ping",
-	"Settings"
+    "Fly",
+    "Speed",
+    "Jump Boost",
+    "Dash",
+    "Dash Power",
+    "Super Auto Click",
+    "Auto Farm",
+    "Auto Quest",
+    "Auto Collect",
+    "Auto Sell",
+    "Auto Fishing",
+    "Perfect Cast",
+    "Perfect Catch",
+    "ESP",
+    "Teleport",
+    "FPS Counter",
+    "Ping",
+    "Settings",
+    "Anti AFK",
+    "Rejoin",
+    "Server Hop",
+    "Performance Mode",
+    "Graphics",
+    "Keybinds",
+    "UI Scale",
+    "Notifications",
+    "Theme",
+    "Debug Mode"
 }
 
-function VoidHub.Start()
-	print("VØID HUB iniciado!")
-	print("Versão: " .. VoidHub.Version)
-end
-
-return VoidHub
